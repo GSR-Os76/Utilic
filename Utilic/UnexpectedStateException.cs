59856baa956da416050824d2df7736a0378b9f0f
GSR-Os76 <122415667+GSR-Os76@users.noreply.github.com>
2024-08-25 13:49:58 -0400

    Add TypeMismatchException and UnexpectedStateException documentation.

--- Utilic/UnexpectedStateException.cs
+++ Utilic/UnexpectedStateException.cs
@@ -1,12 +1,18 @@
 ﻿namespace GSR.Utilic
 {
-
+    /// <summary>
+    /// Exception that occurs when a predicted as impossible state is reached.
+    /// </summary>
     [Serializable]
     public class UnexpectedStateException : Exception
     {
+        /// <inheritdoc/>
         public UnexpectedStateException() { }
+        /// <inheritdoc/>
         public UnexpectedStateException(string message) : base(message) { }
+        /// <inheritdoc/>
         public UnexpectedStateException(string message, Exception inner) : base(message, inner) { }
+        /// <inheritdoc/>
         protected UnexpectedStateException(
           System.Runtime.Serialization.SerializationInfo info,
           System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
